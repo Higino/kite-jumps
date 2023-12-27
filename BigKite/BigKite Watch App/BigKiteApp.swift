2bc9a3346bda0fe10d6c72004b6946fab7518b80
HS <higino@gmail.com>
2023-12-27 11:50:51 +0000

    Initial Commit

---
+++ BigKite/BigKite Watch App/BigKiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigKiteApp.swift
+//  BigKite Watch App
+//
+//  Created by Higino Silva on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BigKite_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigKite/BigKite/BigKiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigKiteApp.swift
+//  BigKite
+//
+//  Created by Higino Silva on 27/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BigKiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
