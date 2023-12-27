bf122be41ed73cb3678560358128010e1f308d68
HS <higino@gmail.com>
2023-12-27 11:51:01 +0000

    Initial commit

--- BigKite/BigKite/ContentView.swift
+++ BigKite/BigKite/ContentView.swift
@@ -1,24 +1,58 @@
-//
-//  ContentView.swift
-//  BigKite
-//
-//  Created by Higino Silva on 27/12/2023.
-//
-
 import SwiftUI
+import CoreMotion
 
 struct ContentView: View {
+    @State private var altitude: Double? = nil
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black // Set the background color to black
+            
+            VStack {
+                Text("Last Height!")
+                    .font(.title)
+                    .foregroundColor(.white) // Set text color to white
+                
+                if let altitude = altitude {
+                    Text("\(String(format: "%.2f", altitude)) meters")
+                        .font(.title)
+                        .foregroundColor(.white) // Set text color to white
+                } else {
+                    Text("N/A")
+                        .font(.title)
+                        .foregroundColor(.white) // Set text color to white
+                }
+                Image(systemName: "arrow.up.circle.fill")
+                    .imageScale(.large) // Increase the icon size
+                    .foregroundStyle(.tint)
+                    .foregroundColor(.white) // Set icon color to white
+                
+            }
+            .padding()
         }
-        .padding()
+        .ignoresSafeArea() // Ignore safe area edges to fill the entire screen
+        .onAppear {
+            startAltitudeUpdates()
+        }
+    }
+    
+    func startAltitudeUpdates() {
+        if CMAltimeter.isRelativeAltitudeAvailable() {
+            let altimeter = CMAltimeter()
+            altimeter.startRelativeAltitudeUpdates(to: .main) { (data, error) in
+                if let altitudeData = data {
+                    let altitude = altitudeData.relativeAltitude.doubleValue
+                    self.altitude = altitude
+                } else {
+                    self.altitude = nil
+                }
+            }
+        }
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
